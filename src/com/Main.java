a3848a38a21a8699ab6ac9268e1a010f06619355
SeungHyeonTak-sub <conficker77@gmail.com>
2021-09-16 07:34:17 +0900

    - 프로그래머스 행렬의 덧셈 문제

---
+++ src/com/Main.java
@@ -0,0 +1,12 @@
+package com;
+
+import com.programmers.level1.MatrixPlus;
+
+public class Main {
+    public static void main(String[] args) {
+        MatrixPlus mp = new MatrixPlus();
+        int[][] arr1 = {{1, 2}, {3, 4}};
+        int[][] arr2 = {{5, 6}, {7, 8}};
+        mp.solution(arr1, arr2);
+    }
+}
