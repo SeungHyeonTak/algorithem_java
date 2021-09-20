299a071badb62bbf047bc8a49465fc2d4009a18f
SeungHyeonTak-sub <conficker77@gmail.com>
2021-09-20 10:02:07 +0900

    - 프로그래머스 소수 찾기 문제

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,11 +1,11 @@
 package com;
 
-import com.programmers.level1.WatermelonProblem;
+import com.programmers.level1.FindingPrime;
 
 public class Main {
     public static void main(String[] args) {
-        WatermelonProblem wp = new WatermelonProblem();
-        int n = 4;
-        wp.solution(n);
+        FindingPrime fp = new FindingPrime();
+        int n = 10;
+        fp.solution(n);
     }
 }
