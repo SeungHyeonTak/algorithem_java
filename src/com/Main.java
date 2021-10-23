c9836870dd01932dac52e72e6cb2feeaf94138d9
SeungHyeonTak-sub <conficker77@gmail.com>
2021-10-23 16:14:44 +0900

    - commit message   - 새로운 아이디 찾기 아직 완성 x   - 중간에 추가된 파일 커밋 테스트

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,11 +1,11 @@
 package com;
 
-import com.programmers.level1.NumberStringAndEnglishWord;
+import com.programmers.level1.RecommendNewID;
 
 public class Main {
     public static void main(String[] args) {
-        NumberStringAndEnglishWord ne = new NumberStringAndEnglishWord();
-        String s = "one4seveneight";
-        ne.solution(s);
+        RecommendNewID rn = new RecommendNewID();
+        String new_id = "...!@BaT#*..y.abcdefghijklm";
+        rn.solution(new_id);
     }
 }
--- src/com/programmers/level1/RecommendNewID.java
+++ src/com/programmers/level1/RecommendNewID.java
@@ -1,2 +1,28 @@
-package com.programmers.level1;public class RecommendNewID {
+package com.programmers.level1;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+// 신규 아이디 추천
+public class RecommendNewID {
+    static final int RECOMMEND_STAGE = 7;
+
+    public String solution(String new_id) {
+        String answer = "";
+        ArrayList<String> strList = new ArrayList<>();
+        new_id = new_id.toLowerCase(); // 1단계 대문자 -> 소문자 치환
+
+        for (int i = 0; i < new_id.length(); i++) {
+            int n = new_id.charAt(i);
+            if ((45 <= n && n <= 57) || (95 == n) || (97 <= n && n <= 122)) {  // 2단계
+//                    System.out.println(strList);
+                strList.add(new_id.split("")[i]);
+            }
+        }
+        // . 처리
+        // 특수문자 제거로 인해 문자열이 0이 될수도 2자 이하가 될수도있다.
+        System.out.println(strList);
+
+        return answer;
+    }
 }
