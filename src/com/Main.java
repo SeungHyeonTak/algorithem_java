e4c221762fee37fc602780af6c6d28a4e6db21a2
SeungHyeonTak-sub <conficker77@gmail.com>
2021-11-10 23:47:08 +0900

    - 프로그래머스 행렬의 곱셈 문제

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,11 +1,16 @@
 package com;
 
-import com.programmers.level2.FibonacciNumber;
+import com.programmers.level2.MatrixMultiplication;
 
 public class Main {
     public static void main(String[] args) {
-        FibonacciNumber fn = new FibonacciNumber();
-        int n = 3;
-        fn.solution(n);
+        MatrixMultiplication m = new MatrixMultiplication();
+//        int[][] arr1 = {{2, 3, 2}, {4, 2, 4}, {3, 1, 4}};
+//        int[][] arr1 = {{1, 4}, {3, 2}, {4, 1}};
+        int[][] arr1 = {{1, 2, 3}, {4, 5, 6}};
+//        int[][] arr2 = {{5, 4, 3}, {2, 4, 1}, {3, 1, 1}};
+//        int[][] arr2 = {{3, 3}, {3, 3}};
+        int[][] arr2 = {{1, 4}, {2, 5}, {3, 6}};
+        m.solution(arr1, arr2);
     }
 }
